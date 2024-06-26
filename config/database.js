b8b1f67d80b135a3f6f90142022244117aded3e4
justin chau <jchau623@gmail.com>
2024-06-26 11:52:34 -0400

    Update configuration for Heroku

--- config/database.js
+++ config/database.js
@@ -1,19 +1,25 @@
 const {
-	db: { username, password, database, host },
-} = require("./index");
+    db: { username, password, database, host },
+  } = require("./index");
 
-module.exports = {
-	development: {
-		username,
-		password,
-		database,
-		host,
-		dialect: "postgres",
-		seederStorage: "sequelize",
-	},
-	production: {
-		use_env_variable: "DATABASE_URL",
-		dialect: "postgres",
-		seederStorage: "sequelize",
-	},
-};
+  module.exports = {
+    development: {
+      username,
+      password,
+      database,
+      host,
+      dialect: "postgres",
+      seederStorage: "sequelize",
+    },
+    production: {
+      use_env_variable: "DATABASE_URL",
+      dialect: "postgres",
+      seederStorage: "sequelize",
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    },
+  };
